de027ad2df44618afb2d3e377ea0672dfc014469
t-bello7 <bello4aus@gmail.com>
2022-10-27 19:15:45 +0100

    add the logic for spliting and joining strings

---
+++ index.rb
@@ -0,0 +1,19 @@
+$mose = {
+  "A":".-", "B":"-...", "C":"-..", "D":"-..", "E":".", F:"..-.", "G":"--.", "H":"....", "J":".---", "K":"-.-", "L":".-..", "M":"--", "N":"-.", "O":"---", "P":".--.", "Q":"--.-", "R":".-.", "S":"...", "T":"-", "U":"..-", "V":"...-", "W":".--", "X":"-..-", "Y":"-.--", "Z":"--.."
+}
+
+
+def decode (var1)
+  ["-- --.--", "-. .- -- ."]
+  ["--", "--.--"]
+  decoded_message = var1.split("   ").map{ |n| n.split(" ").map{|m| 
+    $mose.each{|key, value| if value === m 
+  m = key  
+  end}
+  }.join("")}.join(" ")
+ puts decoded_message
+  # spilt each words in alphabetss
+end
+
+
+decode("-- -.--   -. .- -- .")
